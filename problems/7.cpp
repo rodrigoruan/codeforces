91341bfabc1430d6dcda2a373a3b2f9568e0ba8f
Rodrigo Ruan <rodrigopython16@gmail.com>
2024-03-25 20:08:47 -0300

    7

--- problems/7.cpp
+++ problems/7.cpp
@@ -1,4 +1,4 @@
-// https://codeforces.com/contest/791/problem/A
+// https://codeforces.com/contest/236/problem/A
 
 #include <bits/stdc++.h>
 
@@ -6,5 +6,24 @@ using namespace std;
 
 int main()
 {
+    string c;
+    cin >> c;
+
+    set<char> vals = {};
+
+    for (int i = 0; i < c.size(); i++)
+    {
+        vals.insert(c[i]);
+    }
+
+    if (vals.size() % 2 == 0)
+    {
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else
+    {
+        cout << "IGNORE HIM!" << endl;
+    }
+
     return 0;
 }
