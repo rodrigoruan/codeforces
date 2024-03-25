dce6ec48daaa512e8613d66990db4c6246e2c13c
Rodrigo Ruan <rodrigopython16@gmail.com>
2024-03-25 17:34:17 -0300

    3

---
+++ problems/3.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int y = 0;
+
+    while (a <= b)
+    {
+        a = 3 * a;
+        b = 2 * b;
+        y++;
+    }
+
+    cout << y << endl;
+
+    return 0;
+}
