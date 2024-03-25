8f1cbf4ecba481f5345692ee36d92129cf3916e5
Rodrigo Ruan <rodrigopython16@gmail.com>
2024-03-25 17:26:50 -0300

    2

--- problems/2.cpp
+++ problems/2.cpp
@@ -4,5 +4,35 @@ using namespace std;
 
 int main()
 {
+    int n;
+    string s;
+
+    cin >> n >> s;
+
+    int a = 0, d = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'A')
+            a++;
+        else
+            d++;
+    }
+
+    if (a > d)
+    {
+        cout << "Anton";
+    }
+    else if (a == d)
+    {
+        cout << "Friendship";
+    }
+    else
+    {
+        cout << "Danik";
+    }
+
+    cout << endl;
+
     return 0;
 }
